7290924412d860e6e9590b5f978ccf53e921b596
Stig Rune Jensen <stig.r.jensen@uit.no>
2017-11-21 10:52:24 +0100

    Final serial version

--- src/serial.c
+++ src/serial.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-const double pi_ref = 3.1415926535897932384626433832795;
+const double pi_ref = 3.141592653589793;
 
 void main(int argc, char* argv[]) {
-    printf("\nComputing pi using 1 thread\n");
-
     int n_steps = 1000000000;
     double step = 1.0/n_steps;
     double sum = 0.0;
 
     for (int i = 0; i < n_steps; i++) {
 	double x = (i + 0.5)*step;
-	sum += 1.0/(1.0+x*x);
+        double f_x = 1.0/(1.0+x*x);
+        sum += step*f_x;
     }
 
-    double pi = 4.0*step*sum;
-    printf("Pi:%25.15f\n\n", pi);
+    double pi = 4.0*sum;
+    printf("Pi: %20.15f\n", pi);
+    printf("Ref:%20.15f\n", pi_ref);
 }
