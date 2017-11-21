c7c09248b19fc2da6f3e836c69e14e4e05f87b36
Stig Rune Jensen <stig.r.jensen@uit.no>
2017-11-21 10:56:12 +0100

    Final OpenMP version

--- src/omp.c
+++ src/omp.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
 
-const double pi_ref = 3.1415926535897932384626433832795;
+const double pi_ref = 3.141592653589793;
 
 void main(int argc, char* argv[]) {
-    printf("\nComputing pi using %d threads\n\n", omp_get_max_threads());
-
     int n_steps = 1000000000;
     double step = 1.0/n_steps;
     double sum = 0.0;
 
-#pragma omp parallel firstprivate(n_steps, step) shared(sum)
+#pragma omp parallel firstprivate(n_steps, step) reduction(+:sum)
 {
-    int count = 0;
-#pragma omp for
+#pragma omp for schedule(guided)
     for (int i = 0; i < n_steps; i++) {
 	double x = (i + 0.5)*step;
-	sum += 1.0/(1.0+x*x);
-        count++;
+        double f_x = 1.0/(1.0+x*x);
+        sum += step*f_x;
     }
-    printf("Thread nr %d: %d steps\n", omp_get_thread_num(), count);
 }
 
-    double pi = 4.0*step*sum;
-    printf("\nPi:%25.15f\n\n", pi);
+    double pi = 4.0*sum;
+    printf("Pi: %24.15f\n", pi);
+    printf("Ref:%24.15f\n", pi_ref);
 }
