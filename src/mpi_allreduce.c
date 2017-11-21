1da815e9c74d7b39cb3ccb2ab9dedade4d373d8d
Stig Rune Jensen <stig.r.jensen@uit.no>
2017-11-21 11:03:20 +0100

    Final MPI allreduce version

--- src/mpi_allreduce.c
+++ src/mpi_allreduce.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 
-const double pi_ref = 3.1415926535897932384626433832795;
+const double pi_ref = 3.141592653589793;
 
 void main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
@@ -11,12 +10,9 @@ void main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (my_rank == 0) {
-        printf("\nComputing pi using %d processes\n", world_size);
-    }
-
     int n_steps = 1000000000;
     double step = 1.0/n_steps;
+    double sum = 0.0;
 
     // compute my index range
     int n_steps_per_proc = n_steps/world_size;
@@ -32,15 +28,15 @@ void main(int argc, char* argv[]) {
     double loc_sum = 0.0;
     for (int i = i_start; i < i_end; i++) {
 	double x = (i + 0.5)*step;
-	loc_sum += 1.0/(1.0+x*x);
+	double f_x = 1.0/(1.0+x*x);
+        loc_sum += step*f_x;
     }
 
     // gather contributions from all ranks (all-to-all)
-    double tot_sum = 0.0;
-    MPI_Allreduce(&loc_sum, &tot_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(&loc_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-    double pi = 4.0*step*tot_sum;
-    printf("Rank %d:%25.15f\n", my_rank, pi);
+    double pi = 4.0*sum;
+    printf("Rank %d:%21.15f\n", my_rank, pi);
 
     MPI_Finalize();
 }
